e67135fe65fde557cd1abc3776acaf78b42a08c4
Leudi Rosario <leudi_rosario@hotmail.com>
2018-10-26 12:18:09 -0400

    add-service-worker

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Service Worker
 if ('serviceWorker' in navigator) {
 
-    // navigator.serviceWorker.register('./Savia-sw.js')
-    //     .then(res => console.log('Services Worker Cargado Correctamente', res))
-    //     .catch(err => console.log('Error DB', err));
+    navigator.serviceWorker.register('./Savia-sw.js')
+        .then(res => console.log('Services Worker Cargado Correctamente', res))
+        .catch(err => console.log('Error DB', err));
 
 } else {
     console.log('no puedes usar los services worker')
